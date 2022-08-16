3f401513b7b91653a88ff123ba438adb5b67e74a
dabanya02 <gcciscool@protonmail.com>
2022-08-15 17:52:27 -0700

    fixed css

---
+++ php/webhook.php
@@ -0,0 +1,11 @@
+<?php
+
+if($json = json_decode(file_get_contents("php://input"), true)) {
+    print_r($json);
+    $data = $json;
+} else {
+    print_r($_POST);
+    $data= $_POST;
+}
+
+echo "hello";
